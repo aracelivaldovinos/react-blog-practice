8e851331ee9348ef854a26bff60d875c5a1b937d
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-13 19:38:27 -0500

    add click event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,25 @@
 const Home = () => {
+  const handleClick = (e) =>{
+    console.log('hello', e)
+  }
+  const handleClickAgain = (name, e) =>{
+    console.log(name, e.target)
+  }
   return (  
     <div className="home">
       <h1>Homepage</h1>
+      <button onClick={handleClick}>Click me</button>
+      {/*  
+      const handleClickAgain = (name) =>{
+        console.log(name)
+
+        onClick={() => {handleClickAgain("mario")}} --> anonymous function waits until button is clicked 
+
+        onClick = handleClickAgain("mario") --> function will execute before the button is clicked
+    
+    
+    */}
+      <button onClick={(e) => handleClickAgain("mario", e)}>Click me again</button>
     </div>
   );
 }
