3c8e6229bb6aaddbb4aec35182a63f90605c0594
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-15 13:03:13 -0500

    add delete function and pass to blogslist component as prop

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  // let name = 'cher';
 const [blogs, setBlogs] =useState([
   { title: 'My new website', body: 'lorem ipsum...', author: 'Cher', id: 1 },
   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Elvira', id: 2 },
   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Cher', id: 3 }
 ])
 
+const handleDelete = (id) => {
+  const newBlogs = blogs.filter(((blog) => blog.id !== id ))
+  setBlogs(newBlogs)
+  }
   return (  
     <div className="home">
-      <BlogList blogs={blogs} title= "All Blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'Cher' )} title= "Cher's blogs"/>
+      <BlogList blogs={blogs} title= "All Blogs" handleDelete={handleDelete}/>
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Cher' )} title= "Cher's blogs"/> */}
     </div>
   );
 }
