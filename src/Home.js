ec803da176461da7ec05de90151aadc5a629f652
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-15 12:35:46 -0500

    using props to pass blogs info from Home componenet to BlogList component

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
   // let name = 'cher';
@@ -7,39 +8,10 @@ const [blogs, setBlogs] =useState([
   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Elvira', id: 2 },
   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Cher', id: 3 }
 ])
-// const [name, setName] = useState('Elvira');
-// const [age, setAge] = useState(100);
-// const [initialValue, changingValue] = useState('initialValue')
-//useState returns two values initialValue, changingValue
 
-  // const handleClick = () =>{
-  //   setName('Cher');
-  //   setAge(200)
-  // }
-  // const handleClickAgain = (name, e) =>{
-  //   console.log(name, e.target)
-  // }
   return (  
     <div className="home">
-      {blogs.map((blog)=>(
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>{blog.author}</p>
-        </div>
-      ))}
-      {/* <p>{ name } is { age } years old.</p>
-      <button onClick={handleClick}>Click me</button> */}
-      {/*  
-      const handleClickAgain = (name) =>{
-        console.log(name)
-
-        onClick={() => {handleClickAgain("cher")}} --> anonymous function waits until button is clicked 
-
-        onClick = handleClickAgain("cher") --> function will execute before the button is clicked
-    
-    
-    */}
-      {/* <button onClick={(e) => handleClickAgain("cher", e)}>Click me again</button> */}
+      <BlogList blogs={blogs} title= "All Blogs"/>
     </div>
   );
 }
