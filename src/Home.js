1d865538139eb0131e5f4fcfd4984a9aa0142d95
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-15 12:02:46 -0500

    add map function to display data

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,45 @@
 import { useState } from 'react';
 
 const Home = () => {
-  // let name = 'mario';
-const [name, setName] = useState('Elvira');
-const [age, setAge] = useState (100);
+  // let name = 'cher';
+const [blogs, setBlogs] =useState([
+  { title: 'My new website', body: 'lorem ipsum...', author: 'Cher', id: 1 },
+  { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Elvira', id: 2 },
+  { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Cher', id: 3 }
+])
+// const [name, setName] = useState('Elvira');
+// const [age, setAge] = useState(100);
 // const [initialValue, changingValue] = useState('initialValue')
 //useState returns two values initialValue, changingValue
 
-  const handleClick = () =>{
-    setName('Cher');
-    setAge(200)
-  }
+  // const handleClick = () =>{
+  //   setName('Cher');
+  //   setAge(200)
+  // }
   // const handleClickAgain = (name, e) =>{
   //   console.log(name, e.target)
   // }
   return (  
     <div className="home">
-      <h1>Homepage</h1>
-      <p>{ name } is { age } years old.</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog)=>(
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>{blog.author}</p>
+        </div>
+      ))}
+      {/* <p>{ name } is { age } years old.</p>
+      <button onClick={handleClick}>Click me</button> */}
       {/*  
       const handleClickAgain = (name) =>{
         console.log(name)
 
-        onClick={() => {handleClickAgain("mario")}} --> anonymous function waits until button is clicked 
+        onClick={() => {handleClickAgain("cher")}} --> anonymous function waits until button is clicked 
 
-        onClick = handleClickAgain("mario") --> function will execute before the button is clicked
+        onClick = handleClickAgain("cher") --> function will execute before the button is clicked
     
     
     */}
-      {/* <button onClick={(e) => handleClickAgain("mario", e)}>Click me again</button> */}
+      {/* <button onClick={(e) => handleClickAgain("cher", e)}>Click me again</button> */}
     </div>
   );
 }
