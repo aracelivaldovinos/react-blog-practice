508c859dbcc207ab8fd9452678fc816fd17bd945
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-10-15 14:36:23 -0500

    add handling fetch errors

--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,35 @@ import BlogList from './BlogList';
 const Home = () => {
 const [blogs, setBlogs] =useState(null)
 const [isLoading, setIsLoading] = useState(true)
+const [error, setError] = useState(null)
 
 
 useEffect(()=> {
-  setTimeout(()=>fetch('http://localhost:8000/blogs')
-  .then(res => {
-    return res.json();
-  })
-  .then(data =>{
-    console.log(data);
-    setBlogs(data);
-    setIsLoading(false)
-  }), 1000)
+  setTimeout(()=>{fetch('http://localhost:8000/blogss')
+    .then(response => {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      console.log(response)
+      return response.json();
+    })
+    .then(data =>{
+      console.log(data);
+      setBlogs(data);
+      setIsLoading(false);
+      setError(null);
+    })
+    .catch((error) =>{
+      setError(error.message)
+      setIsLoading(false)
+    } )
+  }, 1000)
 }, [])
 
 //setTimeOut is used to simulate a real fetching of data from a website
   return (  
     <div className="home">
+      {error && <div>{error}.</div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title= "All Blogs"/>}
       {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Cher' )} title= "Cher's blogs"/> */}
